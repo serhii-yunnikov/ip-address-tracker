edc72c6e24cd88042b0741ef9ae1cece0f8eb819
Serhii Yunnikov <yunnikov@icloud.com>
2023-08-14 12:25:41 +0300

    vite -> dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,7 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import App from "./App";
